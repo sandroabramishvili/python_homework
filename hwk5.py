9a62449e9762df917e2fba3b2ce8cfa976fbd420
sandroabramishvili <156215585+sandroabramishvili@users.noreply.github.com>
2026-06-13 14:47:15 +0400

    Add files via upload

---
+++ hwk5.py
@@ -0,0 +1,29 @@
+# დაწერეთ პროგრამა, რომელიც:
+
+# მომხმარებელს შეაყვანინებს ორ რიცხვს.
+# ორივე მნიშვნელობას გადააქცევს float ტიპად.
+# გამოთვლის პირველის მეორეზე გაყოფას.
+
+# გამოიყენეთ try/except/else/finally.
+
+# დაიჭირეთ:
+# ValueError
+# ZeroDivisionError
+# ამოუცნობი ერორი
+# თუ ყველაფერი წარმატებით შესრულდა, გამოტანეთ შედეგი
+# ყოველთვის დაიბეჭდოს: "პროგრამა დასრულდა"
+
+try:
+    num1 = float(input("Enter first number (Positive numbers only): "))
+    num2 = float(input("Enter second number (Positive numbers only): "))
+    result = num1 / num2
+except ValueError:
+    print("Please enter a valid number")
+except ZeroDivisionError:
+    print("You can't divide by zero")
+except Exception as e:
+    print(f"An unknown error ocurred: {e}")
+else:
+    print(f"{num1} / {num2} = {result}")
+finally:
+    print("End of program")
